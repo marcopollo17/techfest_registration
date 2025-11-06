be8b73b3de63ccca9083324475a041eb99dde586
Simuangco <12413149@benilde.edu.ph>
2025-11-06 12:46:16 +0800

    Collected the Participant's Information

--- techfest_registration.py
+++ techfest_registration.py
@@ -4,3 +4,15 @@ print('Event organized by Marco Pollo Simuangco of APPDAET BTCS2')
 num_participants = int(input('How many participants will register? '))
 if num_participants <= 0:
     print('Invalid number of participants')
+else:
+    participants = []
+    for i in range(num_participants):
+        name = input("Enter participant name: ")
+        track = input("Enter chosen track: ")
+        participants.append({'name': name, 'track': track})
+
+    print ('\nRegistered participants:')
+    count = 1
+    for participant in participants :
+        print(f"\t{count}. {participant['name']} - {participant['track']}")
+        count += 1
