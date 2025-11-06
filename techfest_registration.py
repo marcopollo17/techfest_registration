2d7e6fed7700fb31b0060521cb43b7abad482000
Simuangco <12413149@benilde.edu.ph>
2025-11-06 13:06:18 +0800

    Implemented Task Diversity Report

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,3 +1,4 @@
+#TASK 1 - REGISTRATION SETUP
 print('Welcome to SMIT TechFest!')
 print('Event organized by Marco Pollo Simuangco of APPDAET BTCS2')
 
@@ -5,6 +6,7 @@ num_participants = int(input('How many participants will register? '))
 if num_participants <= 0:
     print('Invalid number of participants')
 else:
+#TASK 2 - COLLECT PARTICIPANT INFORMATION
     participants = []
     for i in range(num_participants):
         name = input("Enter participant name: ")
@@ -16,3 +18,10 @@ else:
     for participant in participants :
         print(f"\t{count}. {participant['name']} - {participant['track']}")
         count += 1
+#TASK 3 - TRACK DIVERSITY REPORT
+    unique_tracks = {participant['track'] for participant in participants}
+    if len(unique_tracks) < 2:
+        print('Not enough variety in tracks.')
+    else:
+        tracks_list = ", ".join(unique_tracks)
+        print('\nTracks offered in this event:', tracks_list)
